d42a61d689ca2f0c0b0762bddd54c59e72a34fcb
Josh Lane <lane.joshlane@gmail.com>
2026-09-25 17:20:59 -0700

    Add integration domain boundary: internal/domain/execution.go

---
+++ internal/domain/execution.go
@@ -0,0 +1,76 @@
+package domain
+
+type RootSelector struct {
+	ID          string
+	ProjectName string
+	Directory   string
+	Workspace   string
+}
+
+func (r RootSelector) StableID() string {
+	if r.ID != "" {
+		return r.ID
+	}
+	if r.ProjectName != "" {
+		return r.ProjectName
+	}
+	return r.Directory + "::" + r.Workspace
+}
+
+type PlanRequest struct {
+	Repository  RepositoryRef
+	Ref         string
+	BaseBranch  string
+	PullRequest int64
+	Roots       []RootSelector
+}
+
+type PlanRun struct {
+	Attempts []PlanAttempt
+}
+
+type PlanAttempt struct {
+	RootID        string
+	ProjectName   string
+	Directory     string
+	Workspace     string
+	Status        string
+	Output        string
+	Failure       string
+	ErrorPresent  bool
+}
+
+type ApplyRequest struct {
+	Repository  RepositoryRef
+	Ref         string
+	BaseBranch  string
+	PullRequest int64
+	Roots       []RootSelector
+}
+
+type ApplyRun struct {
+	Attempts []ApplyAttempt
+}
+
+type ApplyAttempt struct {
+	RootID       string
+	ProjectName  string
+	Directory    string
+	Workspace    string
+	Status       string
+	Output       string
+	Failure      string
+	ErrorPresent bool
+}
+
+type ApplyNotification struct {
+	Repository  RepositoryRef
+	PullRequest int64
+	HeadSHA     string
+	HeadRef     string
+	BaseRef     string
+	Actor       string
+	Root        RootSelector
+	Success     bool
+	Source      string
+}
